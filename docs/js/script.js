5db8951b7a26e6d20df5f94dd385aef8e847d941
Sho-Um <73683252+Sho-Um@users.noreply.github.com>
2021-04-04 11:22:56 +0900

    ハンバーガーバークリック時のメニューを閉じる設定

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,7 +1,47 @@
 const btn = document.querySelector('.btn-menu');
 const nav = document.querySelector('.main-nav');
+const list1 = document.querySelector('.list1');
+const list2 = document.querySelector('.list2');
+const list3 = document.querySelector('.list3');
+const list4 = document.querySelector('.list4');
 
 btn.addEventListener('click', () => {
+  nav.classList.toggle('open-menu');
+  if (btn.innerHTML === 'Menu') {
+    btn.innerHTML = 'Close';
+  } else {
+    btn.innerHTML = 'Menu';
+  }
+});
+
+list1.addEventListener('click', () => {
+  nav.classList.toggle('open-menu');
+  if (btn.innerHTML === 'Menu') {
+    btn.innerHTML = 'Close';
+  } else {
+    btn.innerHTML = 'Menu';
+  }
+});
+
+list2.addEventListener('click', () => {
+  nav.classList.toggle('open-menu');
+  if (btn.innerHTML === 'Menu') {
+    btn.innerHTML = 'Close';
+  } else {
+    btn.innerHTML = 'Menu';
+  }
+});
+
+list3.addEventListener('click', () => {
+  nav.classList.toggle('open-menu');
+  if (btn.innerHTML === 'Menu') {
+    btn.innerHTML = 'Close';
+  } else {
+    btn.innerHTML = 'Menu';
+  }
+});
+
+list4.addEventListener('click', () => {
   nav.classList.toggle('open-menu');
   if (btn.innerHTML === 'Menu') {
     btn.innerHTML = 'Close';
