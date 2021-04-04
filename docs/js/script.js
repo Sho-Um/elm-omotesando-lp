0e97ec7d1090a6c788086f8003ed09188fd8f450
Sho-Um <73683252+Sho-Um@users.noreply.github.com>
2021-04-04 11:30:44 +0900

    「エルムクリニック」を押すとメニューが閉じる設定

--- docs/js/script.js
+++ docs/js/script.js
@@ -4,6 +4,7 @@ const list1 = document.querySelector('.list1');
 const list2 = document.querySelector('.list2');
 const list3 = document.querySelector('.list3');
 const list4 = document.querySelector('.list4');
+const logo = document.querySelector('.logo');
 
 btn.addEventListener('click', () => {
   nav.classList.toggle('open-menu');
@@ -43,11 +44,18 @@ list3.addEventListener('click', () => {
 
 list4.addEventListener('click', () => {
   nav.classList.toggle('open-menu');
-  if (btn.innerHTML === 'Menu') {
+  if (btn.inxnerHTML === 'Menu') {
     btn.innerHTML = 'Close';
   } else {
     btn.innerHTML = 'Menu';
   }
+});
+
+logo.addEventListener('click', () => {
+  nav.classList.toggle('open-menu');
+  if (btn.innerHTML === 'Menu') {
+    btn.innerHTML = 'Close';
+  } 
 });
 
 AOS.init();
