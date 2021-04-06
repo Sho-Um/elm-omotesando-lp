2f8f6c69787e3c9936220a966136f6ba9b4c86b6
Sho-Um <73683252+Sho-Um@users.noreply.github.com>
2021-04-06 13:40:52 +0900

    ロゴアニメーション後の背景の色が伸びる設定

--- docs/js/script.js
+++ docs/js/script.js
@@ -81,3 +81,23 @@ $(window).on('load',function(){
 	$("#splash_logo").delay(3000).fadeOut('slow');//ロゴを3秒（3000ms）待機してからフェードアウト
         stroke.play();//SVGアニメーションの実行
 });
+
+
+$(window).on('load',function(){
+  $("#splash-logo").delay(1200).fadeOut('slow');//ロゴを1.2秒でフェードアウトする記述
+
+  //=====ここからローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+  $("#splash").delay(200).fadeOut('slow',function(){//ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
+  
+      $('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
+
+  });
+  //=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+  
+ //=====ここから背景が伸びた後に動かしたいJSをまとめたい場合は
+  $('.splashbg1').on('animationend', function() {    
+      //この中に動かしたいJSを記載
+  });
+  //=====ここまで背景が伸びた後に動かしたいJSをまとめる
+      
+});
